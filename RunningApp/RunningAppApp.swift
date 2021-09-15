c3948037ead978c2067824a9faf4288693ed3e9c
SmithZachary <89857357+SmithZachary@users.noreply.github.com>
2021-09-15 10:37:08 -0500

    Initial Commit

---
+++ RunningApp/RunningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningAppApp.swift
+//  RunningApp
+//
+//  Created by Zach Smith on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct RunningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
